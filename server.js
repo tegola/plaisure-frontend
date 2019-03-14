184105fc960d7098e070062e0429a21897d4e119
Alan Sprecacenere <alan@qreate.it>
2019-03-15 00:13:32 +0100

    Rimosso build automatico all'avvio del server

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const { Nuxt, Builder } = require('nuxt');
+const express = require('express')
+const { Nuxt/*, Builder*/ } = require('nuxt')
 
-const config = require('./nuxt.config.js');
+const config = require('./nuxt.config.js')
 
 // Create new express app
-const app = express();
+const app = express()
 
 // Listen to port 3000 or PORT env if provided
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000)
 
 // Enable production mode
-config.dev = false;
+config.dev = false
 
 // Create instance of nuxt
-const nuxt = new Nuxt(config);
+const nuxt = new Nuxt(config)
 
 // Add nuxt middleware
-app.use(nuxt.render);
+app.use(nuxt.render)
 
 // Build on start
+/*
 new Builder(nuxt).build().catch(err => {
   console.error(err);
   process.exit(1);
 });
+*/
+*
