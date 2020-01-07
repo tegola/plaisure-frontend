15bc3bacc138c6647751bbd37b561fc40f412a08
Alan Sprecacenere <alan@qreate.it>
2020-01-07 01:10:43 +0100

    Modifica scrollable-pane per essere usato con tutti i breakpoint

--- components/scrollable-pane.vue
+++ components/scrollable-pane.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="pg-scrollable-pane">
+	<div :class="isScrollable ? 'pg-scrollable-pane' : null">
 		<slot />
 	</div>
 </template>
@@ -12,33 +12,59 @@ export default {
 		selector: {
 			type: String,
 			default: '*'
+		},
+		breakpoints: {
+			type: Array,
+			default: () => ['xs', 'sm']
 		}
+	},
+
+	computed: {
+		isScrollable() {
+			return (
+				!this.breakpoints.length || this.breakpoints.indexOf(this.$mq) !== -1
+			)
+		}
+	},
+
+	watch: {
+		$mq: 'setContentClass'
 	},
 
 	async mounted() {
 		await this.$nextTick()
 
-		this.$el
-			.querySelector(this.selector)
-			.classList.add('pg-scrollable-pane__content')
+		this.contentNode = this.$el.querySelector(this.selector)
+		this.setContentClass()
+	},
+
+	methods: {
+		setContentClass() {
+			const classList = this.contentNode.classList
+			const contentClass = 'pg-scrollable-pane__content'
+
+			if (this.isScrollable) {
+				classList.add(contentClass)
+			} else {
+				classList.remove(contentClass)
+			}
+		}
 	}
 }
 </script>
 
 <style lang="scss" scoped>
 .pg-scrollable-pane {
-	@include media-breakpoint-down(sm) {
-		& {
-			overflow-x: auto;
-			-webkit-overflow-scrolling: touch;
-		}
-		&__content {
-			white-space: nowrap;
-			flex-wrap: nowrap;
+	& {
+		overflow-x: auto;
+		-webkit-overflow-scrolling: touch;
+	}
+	&__content {
+		white-space: nowrap;
+		flex-wrap: nowrap;
 
-			> * {
-				white-space: normal;
-			}
+		> * {
+			white-space: normal;
 		}
 	}
 }
